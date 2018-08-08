c3b43fad13d4f10546ec29f979ab3026b2ea6d10
florin <florin_postelnicu@yahoo.com>
2018-08-08 05:33:27 -0700

    lesson1

--- Lesson1.py
+++ Lesson1.py
@@ -15,13 +15,43 @@ print('O')
 #     word = word +l[i]
 # print(word)
 
-firstName = input('My First Name is:')
-lastName = input('My Last Name is :')
-name = firstName + " " + lastName
-homeAddress = input('Home Adress :')
-phoneNumber = input('Cell Number :')
-print('So, my visit card is : \n' + name)
-print()
+# firstName = input('My First Name is:')
+# lastName = input('My Last Name is :')
+# name = firstName + " " + lastName
+# homeAddress = input('Home Adress :')
+# phoneNumber = input('Cell Number :')
+# print('So, my visit card is : \n' + name)
+# print()
+#
+# print("Address : " + homeAddress, end=" ")
+# print("Cell : " +phoneNumber)
 
-print("Address : " + homeAddress, end=" ")
-print("Cell : " +phoneNumber)
+# Using Build_in Mathematical Functions
+
+'''
+Absolute Value = abs(x)
+round (x) It rounds up the number x to one decimal
+round(12/7)
+round(x, n) It rounds up number x to n decimals
+round(12/7, 8)
+
+maximum of a sequence of numbers
+max(x1, x2, x3, ...,xn)
+
+minimum of a sequence of numbers
+min(x1,x2,x3,...,xn)
+
+Operators 
+
+ x % n  gives the remainder of the division x:n
+ 
+ x//n  gives the quotient of the division
+
+'''
+print(abs(-3.21))
+
+print(13/4)
+print(round(13/7))
+print(round(13/7, 4))
+print(13//7)
+print(13%7)
