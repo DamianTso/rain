eed03f3c98a83011796a4882f30417eb31168319
florin <florin_postelnicu@yahoo.com>
2018-08-26 10:24:29 -0700

    Lesson arithmetic

--- test001.py
+++ test001.py
@@ -1,11 +1,24 @@
-print("Hello Everybody!")
+
 '''
 Here is a small modification to test git CVS
 '''
 
-print( 2+3 )
 
-'''
-New modification'''
+for row in range(12):
+    for column in range(12-row):
+        print(column, end=" ")
+    print()
 
-print("That's Fine")
+for row in range(12):
+    for column in range(row):
+        for lon in range(12-row):
+            print(' ', end='')
+        print('*', end='')
+    print()
+
+for row in range(12):
+    for column in range(12 - row):
+        print('*', end='')
+    for left in range(row):
+         print(row, end='')
+    print()
