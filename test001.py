c063250844e1959a17614abb92bf22aecbca18b7
florin <florin_postelnicu@yahoo.com>
2018-08-28 06:20:38 -0700

    Lesson arithmetic

--- test001.py
+++ test001.py
@@ -1,6 +1,10 @@
 
 '''
 Here is a small modification to test git CVS
+
+Print using format with a number of digits padding
+
+print("message{:number of digits per column}.format(item to be printed)", *args, **kwargs)
 '''
 
 
@@ -20,10 +24,10 @@ for row in range(12):
     for column in range(12 - row):
         print('*', end='')
     for left in range(row):
-         print(row, end='')
+        print(row, end='')
     print()
-for item in range(100):
-    if item % 7 == 0:
-        print()
-    else:
-        print(item, end=' ')
+for item in range(1,100):
+
+        print("{:6d}".format(item), end=' ')
+        if item % 7 == 0:
+            print("\n")
